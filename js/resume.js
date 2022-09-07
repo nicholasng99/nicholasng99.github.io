8fb21909562cf4cb42c634a4110b62acfa9d19f1
Nicholas Ng <nicholas.ng@ultrastmedtech.com>
2022-09-07 20:26:18 +0800

    fixed js
    culprit was calling $("#about").height() every animation update call, replaced with a local variable that tracks it instead

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,9 @@
 (function (e) {
   ("use strict");
 
+  // Global vars
+  var blossomState = true;
+
   // Enable tooltips
   $(function () {
     $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
@@ -22,163 +25,144 @@
     });
   }
 
-  var blossomState = true;
-  var autoBlossom = true;
-  var mobile = false;
+  function toggleBlossom() {
+    blossomState = !blossomState;
+    $("#blossom-toggle").toggleClass("toggle-on toggle-off");
+  }
+  $("#blossom-toggle").on("click", toggleBlossom);
 
-  // On document ready
-  $(window).on("load", function () {
-    // detecting mobile device
-    mobile = window.matchMedia("(max-width: 767px)").matches;
+  // Blossom animation adapted from confetti code
+  // adapted and rewritten from https://codepen.io/linrock/pen/nMadjQ
+  (function (canvas) {
+    var COLORS,
+      Confetti,
+      NUM_CONFETTI,
+      PI_2,
+      confetti,
+      context,
+      i,
+      curosrX = 0.5,
+      aboutHeight;
 
-    if (mobile) {
-      // auto blossom is false for mobile due to mobile scrolling animation issue
-      autoBlossom = false;
-      toggleBlossom();
+    NUM_CONFETTI = 350;
+
+    COLORS = [
+      [255, 220, 220],
+      [255, 167, 166],
+      [242, 84, 119],
+      [236, 39, 95],
+      [236, 39, 95],
+    ];
+
+    PI_2 = 2 * Math.PI;
+    context = canvas.getContext("2d");
+    window.w = 0;
+    window.h = 0;
+
+    $(window).on("load resize", function () {
+      canvas.width = canvas.height = 0; //reset to 0 first since it can be resized to smaller dimension
+      aboutHeight = $("#about").height(); //local var to track the height of #about section, calling $(...) every update is too slow
+      window.w = canvas.width = $(document).width();
+      return (window.h = canvas.height = $(document).height());
+    });
+
+    function range(a, b) {
+      return (b - a) * Math.random() + a;
     }
 
-    // Blossom animation adapted from confetti code
-    // adapted and rewritten from https://codepen.io/linrock/pen/nMadjQ
-    (function (canvas) {
-      var COLORS,
-        Confetti,
-        NUM_CONFETTI,
-        PI_2,
-        confetti,
-        context,
-        i,
-        curosrX = 0.5;
+    function drawCircle(x, y, r, style) {
+      context.beginPath();
+      context.arc(x, y, r, 0, PI_2, false);
+      context.fillStyle = style;
+      return context.fill();
+    }
 
-      NUM_CONFETTI = 350;
+    $(document).on("mousemove", function (e) {
+      return (curosrX = e.pageX / w);
+    });
 
-      COLORS = [
-        [255, 220, 220],
-        [255, 167, 166],
-        [242, 84, 119],
-        [236, 39, 95],
-        [236, 39, 95],
-      ];
-
-      PI_2 = 2 * Math.PI;
-      context = canvas.getContext("2d");
-      window.w = 0;
-      window.h = 0;
-
-      function resizeCanvas() {
-        canvas.width = canvas.height = 0; //reset to 0 first since it can be resized to smaller dimension
-        window.w = canvas.width = $(document).width();
-        return (window.h = canvas.height = $(document).height());
-      }
-      $(window).on("resize", resizeCanvas);
-      resizeCanvas(); //first resize on load
-
-      function range(a, b) {
-        return (b - a) * Math.random() + a;
+    Confetti = (function () {
+      function Confetti() {
+        this.style = COLORS[~~range(0, 5)];
+        this.rgb =
+          "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
+        this.r = ~~range(3, 7); //size of circles
+        this.r2 = 2 * this.r;
+        this.replace();
       }
 
-      function drawCircle(x, y, r, style) {
-        context.beginPath();
-        context.arc(x, y, r, 0, PI_2, false);
-        context.fillStyle = style;
-        return context.fill();
-      }
+      Confetti.prototype.replace = function () {
+        this.opacity = 0;
+        this.dop = 0.02 / range(1, 4); //opacity rate
+        this.x = range(-this.r2, w - this.r2); //x spread
+        this.y = range(-20, h - this.r2); //y spread
+        this.xmax = w - this.r;
+        this.ymax = h - this.r;
+        this.vx = range(0, 2) - 6 + 5 * curosrX; //x speed
+        return (this.vy = 1 * this.r + range(-1, 3)); //y speed
+      };
 
-      $(document).on("mousemove", function (e) {
-        return (curosrX = e.pageX / w);
-      });
+      Confetti.prototype.draw = function () {
+        var _ref;
+        this.x += this.vx;
+        this.y += this.vy;
+        this.opacity += this.dop;
 
-      Confetti = (function () {
-        function Confetti() {
-          this.style = COLORS[~~range(0, 5)];
-          this.rgb =
-            "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
-          this.r = ~~range(3, 7); //size of circles
-          this.r2 = 2 * this.r;
+        var maxOpacity = Math.max(
+          0.2,
+          Math.min(1, aboutHeight / (this.y * 3 + 1))
+        ); //scales max opacity with y position
+        if (this.opacity > maxOpacity) {
+          this.opacity = maxOpacity;
+          this.dop *= -1;
+        }
+        if ((this.opacity < 0 || this.y > this.ymax) && blossomState) {
           this.replace();
         }
-
-        Confetti.prototype.replace = function () {
-          this.opacity = 0;
-          this.dop = 0.02 / range(1, 4); //opacity rate
-          this.x = range(-this.r2, w - this.r2); //x spread
-          this.y = range(-20, h - this.r2); //y spread
-          this.xmax = w - this.r;
-          this.ymax = h - this.r;
-          this.vx = range(0, 2) - 6 + 5 * curosrX; //x speed
-          return (this.vy = 1 * this.r + range(-1, 3)); //y speed
-        };
-
-        Confetti.prototype.draw = function () {
-          var _ref;
-          this.x += this.vx;
-          this.y += this.vy;
-          this.opacity += this.dop;
-
-          var maxOpacity = Math.max(
-            0.2,
-            Math.min(1, $("#about").height() / (this.y * 3 + 1))
-          ); //scales max opacity with y position
-          if (this.opacity > maxOpacity) {
-            this.opacity = maxOpacity;
-            this.dop *= -1;
-          }
-          if ((this.opacity < 0 || this.y > this.ymax) && blossomState) {
-            this.replace();
-          }
-          if (!(0 < (_ref = this.x) && _ref < this.xmax)) {
-            this.x = (this.x + this.xmax) % this.xmax;
-          }
-          return drawCircle(
-            ~~this.x,
-            ~~this.y,
-            this.r,
-            this.rgb + "," + this.opacity + ")"
-          );
-        };
-
-        return Confetti;
-      })();
-
-      confetti = (function () {
-        var _i, _results;
-        _results = [];
-        for (
-          i = _i = 1;
-          1 <= NUM_CONFETTI ? _i <= NUM_CONFETTI : _i >= NUM_CONFETTI;
-          i = 1 <= NUM_CONFETTI ? ++_i : --_i
-        ) {
-          _results.push(new Confetti());
+        if (!(0 < (_ref = this.x) && _ref < this.xmax)) {
+          this.x = (this.x + this.xmax) % this.xmax;
         }
-        return _results;
-      })();
-
-      window.step = function () {
-        setTimeout(step, 1000 / 60); //callback to self for endless animation, 1000/60 is 60fps
-        var c, _i, _len, _results;
-        context.clearRect(0, 0, w, h);
-        _results = [];
-        for (_i = 0, _len = confetti.length; _i < _len; _i++) {
-          c = confetti[_i];
-          _results.push(c.draw());
-        }
-        return _results;
+        return drawCircle(
+          ~~this.x,
+          ~~this.y,
+          this.r,
+          this.rgb + "," + this.opacity + ")"
+        );
       };
-      if (autoBlossom) {
-        step(); //start the first step
-      }
-    }.call(this, document.getElementById("blossom")));
 
-    function toggleBlossom() {
-      blossomState = !blossomState;
-      if (blossomState && !autoBlossom) {
-        autoBlossom = true;
-        step();
-      }
-      $("#blossom-toggle").toggleClass("toggle-on toggle-off");
-    }
-    $("#blossom-toggle").on("click", toggleBlossom);
+      return Confetti;
+    })();
 
-    // waypoints for resetting values
+    confetti = (function () {
+      var _i, _results;
+      _results = [];
+      for (
+        i = _i = 1;
+        1 <= NUM_CONFETTI ? _i <= NUM_CONFETTI : _i >= NUM_CONFETTI;
+        i = 1 <= NUM_CONFETTI ? ++_i : --_i
+      ) {
+        _results.push(new Confetti());
+      }
+      return _results;
+    })();
+
+    window.step = function () {
+      setTimeout(step, 1000 / 60); //callback to self for endless animation, 1000/60 is 60fps
+      var c, _i, _len, _results;
+      context.clearRect(0, 0, w, h);
+      _results = [];
+      for (_i = 0, _len = confetti.length; _i < _len; _i++) {
+        c = confetti[_i];
+        _results.push(c.draw());
+      }
+      return _results;
+    };
+    step(); //start the first step
+  }.call(this, document.getElementById("blossom")));
+
+  // Waypoint animations
+  $(window).on("load", function () {
+    //for resetting values
     $("#about").waypoint(function () {}, { offset: "0%" });
 
     $("#experience").waypoint(function () {}, { offset: "0%" });
